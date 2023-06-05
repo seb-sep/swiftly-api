6acdd74faeb65ae8d1e59f156d6e4acdc6bccef6
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-06-05 18:26:12 -0400

    add student endpoint

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from schema import User
+from mongoengine import connect
+from pydantic import BaseModel
+
+
+MONGO_URI: str = os.getenv("MONGO_DB_URI")
+connect(MONGO_URI)
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+class StudentAddition(BaseModel):
+    name: str
+
+@app.post("/add/", response_model=StudentAddition)
+async def add_user(user: StudentAddition):
+    new_user = User(name=user.name, notes=[])
+    new_user.save()
+    return user
