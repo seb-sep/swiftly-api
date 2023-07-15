bbd82402b8ee1fa6051cd38a029536b995fa79b3
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-07-15 13:27:32 -0400

    comment out dotenv stuff

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from typing import Annotated
 from bson.json_util import dumps
 
 # only import dotenv if running locally
+'''
 from sys import platform
 if platform == 'darwin':
     from dotenv import load_dotenv
     load_dotenv()
 
+'''
 
 # set up MongoDB connection
 MONGO_URI: str = os.getenv("MONGO_DB_URI")
